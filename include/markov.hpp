63d0efd524890eaebc84399ad831f521487ac10b
lukla <luklabuz@student.agh.edu.pl>
2022-06-16 12:52:35 +0200

    First commit

--- include/markov.hpp
+++ include/markov.hpp
@@ -17,18 +17,25 @@ class MarkovProcess
         using AdjacencyMatrix = std::array<std::array<double, N>, N>;
         using distribution_type = MarkovProcess<T, N>;
 
+    private:
+        std::array<T, N> states_;
+        AdjacencyMatrix probabilities_;
+
+    public:
         MarkovChain() = default;
         MarkovChain(const std::array<T, N>& s, const AdjacencyMatrix& p)
                 : states_(s), probabilities_(p){}
         MarkovChain(const MarkovChain& other)
-                : MarkovChain(other.states_, other.probabilities) {}
-        MarkovChain(MarkovChain&& other)
-                : states_(other.states_), probabilities_(other.probabilities_) {
-            other.states_ = {};
-            other.probabilities_ = {};
+                : MarkovChain(other.states_, other.probabilities_) {}
+        MarkovChain& operator=(const MarkovChain& other)
+        {
+            if(this != &other)
+            {
+                states_ = other.states_;
+                probabilities_ = other.probabilities_;
+            }
+            return *this;
         }
-        MarkovChain& operator=(const MarkovChain& other);
-        MarkovChain& operator=(MarkovChain&& other);
 
         bool operator==(const MarkovChain& rhs)
         {
@@ -41,10 +48,6 @@ class MarkovProcess
             return !(*this == rhs);
         }
 
-    private:
-        std::array<T, N> states_;
-        AdjacencyMatrix probabilities_;
-
         friend class MarkovProcess;
     };
 
@@ -54,15 +57,28 @@ class MarkovProcess
 
 public:
     using result_type = T;
-    using param_type = std::pair<MarkovChain, T>;
+    using param_type = std::pair<MarkovChain, std::size_t>;
 
-    MarkovProcess() = default;
+    MarkovProcess() : chain_({}), current_(0), dist_({}) {}
     MarkovProcess(const std::array<T, N>& states, const typename MarkovChain::AdjacencyMatrix& prob,
                   std::size_t start) : chain_(MarkovChain(states, prob)), current_(start), dist_({}) {
         std::transform(chain_.probabilities_.begin(), chain_.probabilities_.end(), dist_.begin(),
                        [](const std::array<double, N>& r){
             return std::discrete_distribution<unsigned long long>(r.begin(), r.end());
                     });
+    }
+    MarkovProcess(const MarkovProcess& other)
+        : MarkovProcess(other.chain_.states_, other.chain_.probabilities, other.current_) {}
+
+    MarkovProcess& operator=(const MarkovProcess& other)
+    {
+        if(this != &other)
+        {
+            chain_ = other.chain_;
+            current_ = other.current_;
+            dist_ = other.dist_;
+        }
+        return *this;
     }
 
     param_type param()
@@ -82,11 +98,28 @@ public:
         return !(*this == rhs);
     }
 
+    result_type min()
+    {
+        return *chain_.states_.begin();
+    }
+
+    result_type max()
+    {
+        return *chain_.states_.rbegin();
+    }
+
     template<std::uniform_random_bit_generator G>
-    T operator()(G& gen)
+    result_type operator()(G& gen)
     {
         current_ = dist_[current_](gen);
         return chain_.states_[current_];
+    }
+
+    template<std::uniform_random_bit_generator G>
+    result_type operator()(G& gen, param_type param)
+    {
+        std::discrete_distribution<unsigned long long> dist(param.first.probabilities_[param.second]);
+        return param.first.states_[dist(gen)];
     }
 };
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,6 @@
 #include <iostream>
-#include <array>
-#include "markov.hpp"
-#include <random>
 
 int main() {
-    std::array<std::string, 4> a = {"dziekan", "skurwysyn", "robi", "loda"};
-    std::array<std::array<double, 4>, 4> p = { {{0.75, 0.05, 0.15, 0.05},
-                                                {0.3, 0.4, 0.15, 0.15},
-                                                {0.0, 0.5, 0.3, 0.2},
-                                                {0.25, 0.25, 0.25, 0.25}} };
-
-    std::random_device rd;
-    std::mt19937 mt(rd());
-    MarkovProcess<std::string, 4> m(a, p, 1);
-
-    for(int j = 0; j < 10; ++j) {
-        for (int i = 0; i < 10; ++i)
-            std::cout << m(mt) << ' ';
-        std::cout << '\n';
-    }
 
     return 0;
 }
